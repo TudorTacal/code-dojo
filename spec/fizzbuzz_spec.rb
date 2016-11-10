2b8a0a3f02bfd9571a292a2c2fd766aa84661eef
TudorTacal <tudortacal@gmail.com>
2016-11-10 09:16:17 +0000

    Tested for divisibility with 15

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -8,4 +8,8 @@ describe "fizzbuzz" do
   it "should be divisible by 5" do
     expect(divisible_by_5(5)).to eq true
   end
+
+  it "should be divisible by 15" do
+    expect(divisible_by_15(15)).to eq true
+  end
 end
