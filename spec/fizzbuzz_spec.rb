2925f8730cc36e38c767cf09d0417fbdfdc3dad7
TudorTacal <tudortacal@gmail.com>
2016-11-10 09:10:19 +0000

    Tested for divisibility with 5

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,11 @@
+require "fizzbuzz"
+
+describe "fizzbuzz" do
+  it "should be divisible by 3" do
+    expect(divisible_by_3(3)).to eq true
+  end
+
+  it "should be divisible by 5" do
+    expect(divisible_by_5(5)).to eq true
+  end
+end
