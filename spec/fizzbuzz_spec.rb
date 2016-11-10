0f7afe1101fb83363a4bdaad342cabd7af0adce4
TudorTacal <tudortacal@gmail.com>
2016-11-10 09:18:49 +0000

    Tested for non divisibility with 3 5 and 15

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -5,11 +5,23 @@ describe "fizzbuzz" do
     expect(divisible_by_3(3)).to eq true
   end
 
+  it "should not be divisible byu 3" do
+    expect(divisible_by_3(4)).to eq false
+  end
+
   it "should be divisible by 5" do
     expect(divisible_by_5(5)).to eq true
   end
 
+  it "should not be divisible by 5" do
+    expect(divisible_by_5(6)).to eq false
+  end
+
   it "should be divisible by 15" do
     expect(divisible_by_15(15)).to eq true
+  end
+
+  it "should not be divisible by 15" do
+    expect(divisible_by_15(16)).to eq false
   end
 end
