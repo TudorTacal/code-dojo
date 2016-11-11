494b3b74958c95ffa58d05181bfd135003742141
TudorTacal <tudortacal@gmail.com>
2016-11-11 09:44:51 +0000

    Checks if a number is divisible by 15 or not

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -2,5 +2,10 @@ def divisible_by_3(number)
   number%3==0
 end
 
-def divisible_by_5
+def divisible_by_5(number)
+  number%5==0
+end
+
+def divisible_by_15(number)
+  number%15==0
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -8,11 +8,20 @@ describe "Fizzbuzz" do
     it "divisible by 5" do
       expect(divisible_by_5(5)).to eq true
     end
+    it "divisible by 15" do
+      expect(divisible_by_15(15)).to eq true
+    end
 
   end
   context"when number is not" do
     it "divisible by 3" do
       expect(divisible_by_3(4)).to eq false
+    end
+    it "divisible by 5" do
+      expect(divisible_by_5(6)).to eq false
+    end
+    it "divisible by 15" do
+      expect(divisible_by_15(16)).to eq false
     end
   end
 end
