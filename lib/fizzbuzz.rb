ae131d132f7d03371e1d7bd7b8df7d16f45f79e1
TudorTacal <tudortacal@gmail.com>
2016-11-11 09:53:06 +0000

    Checks if number is divisilbe by divisor or not, when divisor is 3

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,15 @@
 def divisible_by_3(number)
-  number%3==0
+  number % 3 == 0
 end
 
 def divisible_by_5(number)
-  number%5==0
+  number % 5 == 0
 end
 
 def divisible_by_15(number)
-  number%15==0
+  number% 15 == 0
+end
+
+def is_divisible_by?(number, divisor)
+  number % divisor == 0
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -13,6 +13,7 @@ describe "Fizzbuzz" do
     end
 
   end
+
   context"when number is not" do
     it "divisible by 3" do
       expect(divisible_by_3(4)).to eq false
@@ -24,4 +25,18 @@ describe "Fizzbuzz" do
       expect(divisible_by_15(16)).to eq false
     end
   end
+
+  context "when number is divisible by divisor" do
+    it "and divisor is 3" do
+      expect(is_divisible_by?(3,3)).to eq true
+    end
+  
+  end
+
+  context "when number is not divisible by divisor" do
+    it "and divisor is 3" do
+      expect(is_divisible_by?(4,3)).to eq false
+    end
+  end
+
 end
